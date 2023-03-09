9c64bea529ad7bf9e8a05704a73b2271b4c21c0a
AAli02 <aali.02p@gmail.com>
2023-03-09 07:51:48 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Aos from "aos";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Nav, Landing, About, Services, Footer, Test } from "./components";
 
+import 'aos/dist/aos.css';
 import "./App.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
+
+  useEffect(() => {
+    Aos.init({duration: 300});
+  }, [])
+  
   const sectionRefs = useRef([]);
 
   useEffect(() => {
     const sections = sectionRefs.current;
-
+  
     sections.forEach((section, i) => {
       const prevColor = i === 0 ? "#f15946" : sections[i - 1].dataset.bgColor;
-
+  
       gsap.to(section, {
         scrollTrigger: {
           trigger: section,
-          markers: true,
-          start: "top center+=25%",
+          markers: false,
+          end: () => `+=${section.offsetHeight}`,
+          scrub: 1,
+          snap: 1 / (sections.length - 1),
           onEnter: () =>
             gsap.to("body", {
               backgroundColor: section.dataset.bgColor,
@@ -46,35 +55,36 @@ const App = () => {
       <Router>
         <Routes></Routes>
       </Router>
-        <Nav />
-        <Landing />
-        <About />
-        <Services />      <section
+      <Nav />
+      {/* <Landing /> */}
+      {/* <About /> */}
+      {/* <Services />{" "} */}
+      <a href="#one" className="scroll">
+        <div className="scroll__icon"></div>
+      </a>
+      <section
         ref={(el) => (sectionRefs.current[0] = el)}
         className="o-section"
       >
-        <h1></h1>
       </section>
-      <section
+      <section id="one"
         ref={(el) => (sectionRefs.current[1] = el)}
         className="o-section js-section"
         data-bg-color="#f15946"
       >
-        <h3></h3>
       </section>
       <section
         ref={(el) => (sectionRefs.current[2] = el)}
         className="o-section js-section"
         data-bg-color="#81a5ce"
       >
-        <h3></h3>
       </section>
       <section
         ref={(el) => (sectionRefs.current[3] = el)}
         className="o-section js-section"
         data-bg-color="#636567"
       ></section>
-        <Footer />
+      <Footer />
     </div>
   );
 };
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,49 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import "./Nav.css"
+import "./Nav.css";
 
 function Nav() {
   return (
     <nav>
-    <figure>
-      {/* <img id="personal_logo"  alt="" /> */}
-      <h1 id='personal_logo'>Web Forte</h1>
-    </figure>
-    <ul className="nav__link--list">
-      <li className="nav__link">
-        <a 
-          href="#about"
-          className="nav__link--anchor link__hover-effect">
-        About
-        </a>
-      </li>
-      {/* <li className="nav__link">
-        <a 
-          href="#services"
-          className="nav__link--anchor link__hover-effect">
-        Services
-        </a>
-      </li> */}
-      <li className="nav__link">
-        <a 
-          href="#work"
-          className="nav__link--anchor link__hover-effect">
-        Work
-        </a>
-      </li>
-      <li className="nav__link">
-        <a 
-          href="#chat"
-          className="contact__me">
-        Let's Chat!
-        </a>
-      </li>
-    </ul>
-  </nav>
-  )
+      <figure>
+        <h1 id="personal_logo">Web Forte</h1>
+      </figure>
+      <ul className="nav__link--list">
+        <li  className="nav__link nav__1">
+          <a href="#about" className="nav__link--anchor link__hover-effect">
+            About
+          </a>
+        </li>
+        <li className="nav__link nav__2">
+          <a href="#work" className="nav__link--anchor link__hover-effect">
+            Services
+          </a>
+        </li>
+        <li className="nav__link nav__3">
+          <a href="#chat" className="contact__me">
+            Let's Chat!
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
-
-
-
+export default Nav;
