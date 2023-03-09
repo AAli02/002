385c89969aa37b0eddbffb7941c0a042a9783660
AAli02 <aali.02p@gmail.com>
2023-03-09 10:40:39 -0500

    add

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const App = () => {
       <Router>
         <Routes></Routes>
       </Router>
-      {/* <Nav /> */}
+      <Nav />
       <Home />
       {/* <Landing /> */}
       {/* <About /> */}
